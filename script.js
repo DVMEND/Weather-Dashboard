6f1895322aeb5bdf3c3f5a5048b52e1652355005
David Mendoza <davidmendoza0101@gmail.com>
2021-08-10 07:40:23 -0700

    Fixed the javascript so that the 5-day forecast is produced dynamically. Used bootstrap cards for each day. Provided code that stores the response from the OpenWeather APIs in local storage. Still need to code a way to call that code when a history item is pressed.

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+$(document).ready(function(){
+  $("#search-button").click(function(event){
+    event.preventDefault();
+    var cityInput = $("#search-value").val();
+    var noSpace = cityInput.replace(/\s/g, '+')
+    console.log(noSpace);
+    //apiKey= "62e3185f6eb6074c28be5645cf545d09";
 
-$("#search-button").click(function(event){
-  event.preventDefault();
-  var cityInput = $("#search-value").val();
-  var noSpace = cityInput.replace(/\s/g, '+')
-  console.log(noSpace);
-  //apiKey= "62e3185f6eb6074c28be5645cf545d09";
-
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + noSpace + "&units=imperial&appid=62e3185f6eb6074c28be5645cf545d09";
-  console.log(queryURL); 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + noSpace + "&units=imperial&appid=62e3185f6eb6074c28be5645cf545d09";
+    console.log(queryURL); 
 
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+        $("#today").addClass("border");
+        $("#today p").empty();
+        $("#today h2").empty();
+        $("#cards").empty();
 
         $("#history").append("<li>" + cityInput + "</li>");
-        var cityName= response.name;
 
+        var cityName= response.name;
         var currentDate = moment()
         var currentDate = moment().format("MM/DD/YYYY");
         var iconCode = response.weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
         $('icon').attr('src', iconUrl);
 
         var cityTemperature = response.main.temp;
@@ -43,22 +48,60 @@ $("#search-button").click(function(event){
         $("#today").append("<p>Humidity: " + cityHumidity + "%</p>");
         $("#today").append("<p>Wind Speed: " + windSpeed + " MPH</p>");
         
+        localStorage.setItem(noSpace + "Current", JSON.stringify(response));
       });
+      
 
-
-})
-$("#search-button").click(function(event){
-  event.preventDefault();
-  var cityInput = $("#search-value").val();
-  var noSpace = cityInput.replace(/\s/g, '+')
-  
-  var queryURL = 
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response){
 
   })
-})
+  $("#search-button").click(function(event){
+    event.preventDefault();
+    var cityInput = $("#search-value").val();
+    var noSpace = cityInput.replace(/\s/g, '+')
+    
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + noSpace + "&units=imperial&appid=62e3185f6eb6074c28be5645cf545d09";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      $("#5day").text("5-Day Forecast:")
+      
+    
+
+      for (var i=0; i<response.list.length; i++){
+        if (response.list[i].dt_txt.includes("12:00:00")){
+        
+        var dates = response.list[i].dt_txt;
+        var datesFormat = moment(dates);
+        var datesFormat = moment(dates).format("MM/DD/YYYY");
+        var datesTemp = response.list[i].main.temp;
+        var datesHumidity = response.list[i].main.humidity;
+        var datesIcon = response.list[i].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + datesIcon + ".png";
+        console.log(iconURL);
+        console.log(datesIcon);
+
+
+        $("#cards").append(
+          '<div id="card' + i + '" class="card bg-info mx-3" style="width: 10rem;"><div class="card-body"><h5 class="card-title">Card title</h5><img src="" alt="Weather Icon"><p class="card-text temp">Temp: </p><p class="card-text humidity">Humidity: </p></div></div>'
+        );
+        $("#card"+i+" h5").text(datesFormat);
+        $("#card"+i).find(".temp").append(datesTemp + "℉");
+        $("#card"+i).find(".humidity").append(datesHumidity+"%");
+        $("#card"+i).find("img").attr("src",iconURL);
+      }}
+      localStorage.setItem(noSpace + "5Day", JSON.stringify(response));
+
+      console.log(queryURL);
+    
+
+      console.log(datesTemp);
+
+      console.log(datesFormat);
+       
+    })
+  })
+
+});
 
